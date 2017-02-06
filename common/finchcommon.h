a4b699220c81854ac1a2a3fa13b598c1329c92e0
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-02-07 09:01:58 +1100

    Add static library

---
+++ common/finchcommon.h
@@ -0,0 +1,6 @@
+#ifndef FINCHCOMMON_H
+#define FINCHCOMMON_H
+
+void * print_color(void * data);
+
+#endif
---
+++ common/print.c
@@ -0,0 +1,13 @@
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include "finchcommon.h"
+
+#include <stdio.h>
+
+void * print_color(void * data)
+{
+  printf("Hello from %s!\n", (const char *)data);
+  return 0;
+}
